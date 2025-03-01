0c1b64ea61d5c6573d37286ac1c33713d1a38444
Ayoub Ghriss <dev@ayghri.com>
2025-03-01 14:43:24 -0700

    to satisfy pylint

--- sphedron/utils/__init__.py
+++ sphedron/utils/__init__.py
@@ -1,3 +1,16 @@
+"""
+Author: Ayoub Ghriss, dev@ayghri.com
+Date: 2024
+
+License: Non-Commercial Use Only
+
+Permission is granted to use, copy, modify, and distribute this software
+for non-commercial purposes only, with attribution to the original author.
+Commercial use requires explicit permission.
+
+This software is provided "as is", without warranty of any kind.
+"""
+
 from .helpers import connect_nodes
 from .helpers import faces_to_edges
 from .helpers import get_rotation_matrices
