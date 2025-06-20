9a713ebf22a652a405b775d7524b82cbace48fd4
Ayoub G. <git@ayghri.com>
2025-06-20 11:30:06 -0600

    added docstring

--- sphedron/mesh/__init__.py
+++ sphedron/mesh/__init__.py
@@ -1,3 +1,13 @@
+"""
+License: Non-Commercial Use Only
+
+Permission is granted to use, copy, modify, and distribute this software
+for non-commercial purposes only, with attribution to the original author.
+Commercial use requires explicit permission.
+
+This software is provided "as is", without warranty of any kind.
+"""
+
 from .base import NodesOnlyMesh
 from .refinables import Cubesphere
 from .refinables import Icosphere
